f532ac5f64d52bdb65720768d7417ea7ff7b6314
Yun <oys0817@gmail.com>
2018-11-15 14:01:45 -0500

    first Yun's commit

--- huake.cpp
+++ huake.cpp
@@ -1,6 +1,7 @@
 #include "huakelib.h"
 #include "fssimplewindow.h"
 
+// Yun is here
 // holds pointers for rendering everything 
 class MainData
 {
@@ -70,6 +71,8 @@ int main(void)
     // cube1.SetPlanePos(0.,0.,-400.); // x y z
     // cube2.SetPlanePos(0.,0.,-400.);
 
+	printf("Yun is here!\n"); 
+	
     Sprite cubes[24];
     // set local position of the cubes
     cubes[0].SetPos(90.,10.,-90.);
